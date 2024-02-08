6edb2a3c9024029956e6694fad0a4938498a1690
onotuamen <onosa.itua@icloud.com>
2024-02-07 17:20:37 -0800

    safe print listt

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
+
 def safe_print_list(my_list=[], x=0):
     num = 0
-    for i in range(x):
     try:
-    print(my_list[i], end="")
-    num += 1
+        for item in range(x):
+            print("{}".format(my_list[item]), end='')
+            num += 1
     except IndexError:
-    break
-    print("")
-    Return
-
+        pass
+    print()
+    return num
