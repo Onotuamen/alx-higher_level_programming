beb8f85142d237543a00ddb3552fba646c59a41c
onotuamen <onosa.itua@icloud.com>
2024-02-13 19:04:21 -0800

    edited code

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,15 +1,75 @@
 #!/usr/bin/python3
-Rectangle = __import__('3-rectangle').Rectangle
+"""Module contains a class Rectangle"""
 
-my_rectangle = Rectangle(2, 4)
-print("Area: {} - Perimeter: {}".format(my_rectangle.area, my_rectangle.perimeter()))
 
-print(str(my_rectangle))
-print(repr(my_rectangle))
+class Rectangle:
+    """Rectangle class definition"""
 
-print("--")
+    def __init__(self, width=0, height=0):
+        """
+        Initialize the Rectangle class
+        :param width: (int) width of the rectangle
+        :param height: (int) height of the rectangle
+        """
+        self.width = width
+        self.height = height
 
-my_rectangle.width = 10
-my_rectangle.height = 3
-print(my_rectangle)
-print(repr(my_rectangle))
+    @property
+    def width(self):
+        """Return the width of the rectangle"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        Set the width of the rectangle
+        :param value: (int) new width of the rectangle
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """Return the height of the triangle"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        Set the height of the rectangle
+        :param value: (int) new height of the rectangle
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        """
+        Return the area of the rectangle
+        :return: (int) the area of the rectangle
+        """
+        return self.__width * self.height
+
+    def perimeter(self):
+        """
+        Return the perimeter of the rectangle
+        :returns: (int) the perimeter of the rectangle
+        """
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * self.__width + 2 * self.__height
+
+    def __str__(self):
+        """Draw string representation of the rectangle with #
+        :return: (str) the string representation with #
+        """
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        return "\n".join(('#' * self.width) for _ in range(self.height))
